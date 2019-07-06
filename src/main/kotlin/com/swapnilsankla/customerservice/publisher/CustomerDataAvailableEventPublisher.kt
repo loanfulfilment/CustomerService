dcad2c0d8f0b697e23fe92f76de0c98cf055f037
Swapnil Sankla <swapnil.sankla@gmail.com>
2019-07-06 23:41:53 +0530

    Add log message while publishing customer details

--- src/main/kotlin/com/swapnilsankla/customerservice/publisher/CustomerDataAvailableEventPublisher.kt
+++ src/main/kotlin/com/swapnilsankla/customerservice/publisher/CustomerDataAvailableEventPublisher.kt
@@ -1,16 +1,20 @@
 package com.swapnilsankla.customerservice.publisher
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.swapnilsankla.customerservice.listener.NeedLoanEventListener
 import com.swapnilsankla.customerservice.model.Customer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
+import java.util.logging.Logger
 
 @Component
 class CustomerDataAvailableEventPublisher(@Autowired val kafkaTemplate: KafkaTemplate<String, String>,
                                           @Autowired val objectMapper: ObjectMapper) {
 
     fun publish(customer: Customer) {
+        Logger.getLogger(CustomerDataAvailableEventPublisher::class.simpleName).info("raising event $customer")
+
             kafkaTemplate.send("customerDataAvailableForLoanProcessing",
                 objectMapper.writeValueAsString(customer))
     }
