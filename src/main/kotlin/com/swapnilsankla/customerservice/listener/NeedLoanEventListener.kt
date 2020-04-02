3ab24ffa62126070d6ff71f62d5004e9eeaff3da
Swapnil Sankla <swapnil.sankla@gmail.com>
2020-04-02 11:44:51 +0530

    Send application number as uber-trace-id for distributed tracing across services

--- src/main/kotlin/com/swapnilsankla/customerservice/listener/NeedLoanEventListener.kt
+++ src/main/kotlin/com/swapnilsankla/customerservice/listener/NeedLoanEventListener.kt
@@ -3,8 +3,10 @@ package com.swapnilsankla.customerservice.listener
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.swapnilsankla.customerservice.publisher.CustomerDataAvailableEventPublisher
 import com.swapnilsankla.customerservice.repository.CustomerRepository
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.messaging.handler.annotation.Headers
 import org.springframework.stereotype.Component
 import java.util.logging.Logger
 
@@ -13,15 +15,17 @@ class NeedLoanEventListener(@Autowired val repository: CustomerRepository,
                             @Autowired val objectMapper: ObjectMapper,
                             @Autowired val customerDataAvailableEventPublisher: CustomerDataAvailableEventPublisher) {
 
-    @KafkaListener(topics = ["needLoanEvent"])
-    fun listen(needLoanEventString: String) {
-        val needLoanEvent = objectMapper.readValue(needLoanEventString, NeedLoanEvent::class.java)
-        Logger.getLogger(NeedLoanEventListener::class.simpleName)
-                .info("needLoanEvent event received for customer ${needLoanEvent.customerId}")
+    val logger = LoggerFactory.getLogger(NeedLoanEventListener::class.simpleName)
 
-        repository.
-                findByCustomerId(needLoanEvent.customerId)
-                .doOnSuccess(customerDataAvailableEventPublisher::publish)
+    @KafkaListener(topics = ["needLoanEvent"])
+    fun listen(needLoanEventString: String, @Headers headers: Map<String, Any>) {
+        val needLoanEvent = objectMapper.readValue(needLoanEventString, NeedLoanEvent::class.java)
+        logger.info("needLoanEvent event received for customer ${needLoanEvent.customerId}")
+
+        val traceId = headers["uber-trace-id"] as? ByteArray ?: ByteArray(1)
+
+        repository.findByCustomerId(needLoanEvent.customerId)
+                .doOnSuccess { customerDataAvailableEventPublisher.publish(it, traceId) }
                 .subscribe()
     }
 }
--- src/main/kotlin/com/swapnilsankla/customerservice/publisher/CustomerDataAvailableEventPublisher.kt
+++ src/main/kotlin/com/swapnilsankla/customerservice/publisher/CustomerDataAvailableEventPublisher.kt
@@ -3,6 +3,8 @@ package com.swapnilsankla.customerservice.publisher
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.swapnilsankla.customerservice.listener.NeedLoanEventListener
 import com.swapnilsankla.customerservice.model.Customer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.header.internals.RecordHeader
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
@@ -12,10 +14,22 @@ import java.util.logging.Logger
 class CustomerDataAvailableEventPublisher(@Autowired val kafkaTemplate: KafkaTemplate<String, String>,
                                           @Autowired val objectMapper: ObjectMapper) {
 
-    fun publish(customer: Customer) {
-        Logger.getLogger(CustomerDataAvailableEventPublisher::class.simpleName).info("raising event $customer")
+    private val logger = Logger.getLogger(CustomerDataAvailableEventPublisher::class.simpleName)
 
-            kafkaTemplate.send("customerDataAvailableForLoanProcessing",
-                objectMapper.writeValueAsString(customer))
+    fun publish(customer: Customer, traceId: ByteArray) {
+        val message = buildMessage(customer, traceId)
+        logger.info("publishing message $message for customer $customer")
+        kafkaTemplate.send(message)
+    }
+
+    private fun buildMessage(customer: Customer, traceId: ByteArray): ProducerRecord<String, String> {
+        val message = ProducerRecord<String, String>(
+                "customerDataAvailableForLoanProcessing",
+                objectMapper.writeValueAsString(customer)
+        )
+        message.headers().remove("uber-trace-id")
+        message.headers().add("uber-trace-id", traceId)
+
+        return message
     }
 }
