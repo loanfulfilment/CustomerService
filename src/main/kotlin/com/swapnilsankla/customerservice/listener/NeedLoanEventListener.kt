3deb7d47f97ca7ba684c2891d9f680759446fe06
Swapnil Sankla <swapnil.sankla@gmail.com>
2020-04-18 17:00:35 +0530

    Depend on starter to do trace and kafka event publishing

--- src/main/kotlin/com/swapnilsankla/customerservice/listener/NeedLoanEventListener.kt
+++ src/main/kotlin/com/swapnilsankla/customerservice/listener/NeedLoanEventListener.kt
@@ -3,17 +3,18 @@ package com.swapnilsankla.customerservice.listener
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.swapnilsankla.customerservice.publisher.CustomerDataAvailableEventPublisher
 import com.swapnilsankla.customerservice.repository.CustomerRepository
+import com.swapnilsankla.tracestarter.TraceIdExtractor
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Headers
 import org.springframework.stereotype.Component
-import java.util.logging.Logger
 
 @Component
 class NeedLoanEventListener(@Autowired val repository: CustomerRepository,
                             @Autowired val objectMapper: ObjectMapper,
-                            @Autowired val customerDataAvailableEventPublisher: CustomerDataAvailableEventPublisher) {
+                            @Autowired val customerDataAvailableEventPublisher: CustomerDataAvailableEventPublisher,
+                            @Autowired val traceIdExtractor: TraceIdExtractor) {
 
     val logger = LoggerFactory.getLogger(NeedLoanEventListener::class.simpleName)
 
@@ -22,10 +23,10 @@ class NeedLoanEventListener(@Autowired val repository: CustomerRepository,
         val needLoanEvent = objectMapper.readValue(needLoanEventString, NeedLoanEvent::class.java)
         logger.info("needLoanEvent event received for customer ${needLoanEvent.customerId}")
 
-        val traceId = headers["uber-trace-id"] as? ByteArray ?: ByteArray(1)
+        val trace = traceIdExtractor.fromKafkaHeaders(headers)
 
         repository.findByCustomerId(needLoanEvent.customerId)
-                .doOnSuccess { customerDataAvailableEventPublisher.publish(it, traceId) }
+                .doOnSuccess { customerDataAvailableEventPublisher.publish(it, trace) }
                 .subscribe()
     }
 }
--- src/main/kotlin/com/swapnilsankla/customerservice/publisher/CustomerDataAvailableEventPublisher.kt
+++ src/main/kotlin/com/swapnilsankla/customerservice/publisher/CustomerDataAvailableEventPublisher.kt
@@ -1,35 +1,19 @@
 package com.swapnilsankla.customerservice.publisher
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.swapnilsankla.customerservice.listener.NeedLoanEventListener
 import com.swapnilsankla.customerservice.model.Customer
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.header.internals.RecordHeader
+import com.swapnilsankla.tracestarter.CustomKafkaTemplate
+import com.swapnilsankla.tracestarter.Trace
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
-import java.util.logging.Logger
 
 @Component
-class CustomerDataAvailableEventPublisher(@Autowired val kafkaTemplate: KafkaTemplate<String, String>,
-                                          @Autowired val objectMapper: ObjectMapper) {
+class CustomerDataAvailableEventPublisher(@Autowired val kafkaTemplate: CustomKafkaTemplate) {
 
-    private val logger = Logger.getLogger(CustomerDataAvailableEventPublisher::class.simpleName)
-
-    fun publish(customer: Customer, traceId: ByteArray) {
-        val message = buildMessage(customer, traceId)
-        logger.info("publishing message $message for customer $customer")
-        kafkaTemplate.send(message)
-    }
-
-    private fun buildMessage(customer: Customer, traceId: ByteArray): ProducerRecord<String, String> {
-        val message = ProducerRecord<String, String>(
-                "customerDataAvailableForLoanProcessing",
-                objectMapper.writeValueAsString(customer)
+    fun publish(customer: Customer, trace: Trace) {
+        kafkaTemplate.publish(
+                topic = "customerDataAvailableForLoanProcessing",
+                data = customer,
+                trace = trace
         )
-        message.headers().remove("uber-trace-id")
-        message.headers().add("uber-trace-id", traceId)
-
-        return message
     }
 }
